db27b63e1a2e15a908d9456b04034c4c4feb1547
mohajel <msadegi80@yahoo.com>
2022-11-05 17:42:00 +0330

    just testing fork

--- sysfile.c
+++ sysfile.c
@@ -469,7 +469,63 @@ sys_pipe(void)
 	return 0;
 }
 
+// int openfile(char* path, int omode)
+// {
+// 	char* path;
+// 	int fd, omode;
+// 	struct file* f;
+// 	struct inode* ip;
+
+// 	begin_op();
+
+// 	if (omode & O_CREATE)
+// 	{
+// 		ip = create(path, T_FILE, 0, 0);
+// 		if (ip == 0)
+// 		{
+// 			end_op();
+// 			return -1;
+// 		}
+// 	}
+// 	else
+// 	{
+// 		if ((ip = namei(path)) == 0)		
+// {
+// 			end_op();
+// 			return -1;
+// 		}
+// 		ilock(ip);
+// 		if (ip->type == T_DIR && omode != O_RDONLY)
+// 		{
+// 			iunlockput(ip);
+// 			end_op();
+// 			return -1;
+// 		}
+// 	}
+
+// 	if ((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0)
+// 	{
+// 		if (f)
+// 			fileclose(f);
+// 		iunlockput(ip);
+// 		end_op();
+// 		return -1;
+// 	}
+// 	iunlock(ip);
+// 	end_op();
+
+// 	f->type = FD_INODE;
+// 	f->ip = ip;
+// 	f->off = 0;
+// 	f->readable = !(omode & O_WRONLY);
+// 	f->writable = (omode & O_WRONLY) || (omode & O_RDWR);
+// 	return fd;
+// }
+
+
 int sys_change_file_size(void)
 {
+	// int pid = open("file1.txt", O_CREATE | O_RDWR);
+	// close(pid);
 	return 78;
 }
